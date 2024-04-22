b6a12a302b6501abae3e23b74ef5a56c11d3724b
hhuezo <hugo.alex.huezo@gmail.com>
2024-04-22 13:59:06 -0600

    redireccion login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\web\PrestamoWebController;
 use App\Http\Controllers\web\ReciboWebController;
 use Illuminate\Support\Facades\Route;
@@ -15,11 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
