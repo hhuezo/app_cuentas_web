e31398e0254bbc1379a227d577a9e8f9af638123
hhuezo <hugo.alex.huezo@gmail.com>
2024-04-22 07:54:37 -0600

    plantilla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('username Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                @error('email')
+                                @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -70,4 +70,107 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+
+
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <!--Title-->
+    <title>YashAdmin -Sales Management System Admin Dashboard Bootstrap HTML Template | DexignZone</title>
+
+    <!-- FAVICONS ICON -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('template/images/favicon.png') }}">
+    <link href="{{ asset('template/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
+    <link class="main-css" href="{{ asset('template/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body style="background-image:url('images/bg.png'); background-position:center;">
+    <div class="authincation fix-wrapper">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <div class="text-center mb-3">
+                                        <a href="index.html"><img src="images/logo/logo-full.png" alt=""></a>
+                                    </div>
+
+
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    <h4 class="text-center mb-4">Inicio de sesión</h4>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="mb-1 form-label">Usuario</label>
+                                            <input id="username" type="text"
+                                                class="form-control @error('username') is-invalid @enderror"
+                                                name="username" value="{{ old('username') }}" required
+                                                autocomplete="username" autofocus>
+
+                                            @error('username')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="mb-3 position-relative">
+                                            <label class="form-label" for="dz-password">Password</label>
+                                            <input type="password" id="dz-password" name="password" required class="form-control">
+                                            <span class="show-pass eye">
+                                                <i class="fa fa-eye-slash"></i>
+                                                <i class="fa fa-eye"></i>
+                                            </span>
+                                        </div>
+                                        <div class="form-row d-flex flex-wrap justify-content-between mb-2">
+                                            {{-- <div class="form-group mb-sm-4 mb-1">
+                                                <div class="form-check custom-checkbox ms-1">
+                                                    <input type="checkbox" class="form-check-input" id="basic_checkbox_1">
+                                                    <label class="form-check-label" for="basic_checkbox_1">Remember my preference</label>
+                                                </div>
+                                            </div> --}}
+                                            {{-- <div class="form-group ms-2">
+                                                <a class="text-hover" href="page-forgot-password.html">Forgot Password?</a>
+                                            </div> --}}
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                        </div>
+                                    </form>
+                                    {{-- <div class="new-account mt-3">
+                                        <p>Already have an account? <a class="text-primary" href="page-register.html">Sign Up</a></p>
+                                    </div> --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--**********************************
+ Scripts
+***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('template/vendor/global/global.min.js') }}"></script>
+
+    <script src="{{ asset('template/vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('template/js/custom.min.js') }}"></script>
+    {{-- <script src="{{ asset('template/js/deznav-init.js') }}"></script> --}}
+
+
+
+</body>
+
+</html>
