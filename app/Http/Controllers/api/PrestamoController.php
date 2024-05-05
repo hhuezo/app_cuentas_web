66a3e2456b6e21b4c888e9c1d0bc18085f125115
hhuezo <hugo.alex.huezo@gmail.com>
2024-05-05 11:05:26 -0600

    calculo cuota semanal

--- app/Http/Controllers/api/PrestamoController.php
+++ app/Http/Controllers/api/PrestamoController.php
@@ -35,37 +35,39 @@ class PrestamoController extends Controller
             }
 
             $prestamos = DB::table('prestamo')
-                ->select(
-                    'prestamo.id',
-                    DB::raw('LPAD(prestamo.codigo, 3, "0") as codigo'),
-                    DB::raw('FORMAT(prestamo.cantidad, 2) as cantidad'),
-                    DB::raw('FORMAT(prestamo.interes, 2) as interes'),
-                    'prestamo.estado',
-                    'prestamo.amortizacion',
-                    'prestamo.id as comprobante',
-                    'prestamo.administrador',
-                    'prestamo.pago_especifico as pagoEspecifico',
-                    'persona.nombre as persona',
-                    'tipo_pago.nombre as tipoPago',
-                    'prestamo.tipo_pago_id',
-                    'prestamo.numero_pagos',
-                    'prestamo.pago_especifico',
-                    DB::raw('IFNULL(prestamo.observacion, "") as observacion'),
-                    DB::raw('DATE_FORMAT(prestamo.fecha, "%d/%m/%Y") as fecha'),
-                    DB::raw('IFNULL((SELECT remanente FROM recibo WHERE recibo.prestamo_id = prestamo.id and recibo.estado = 2  ORDER BY recibo.id DESC LIMIT 1), prestamo.cantidad) AS deuda'),
-                    DB::raw('ROUND((prestamo.cantidad / prestamo.numero_pagos) + (prestamo.cantidad * (prestamo.interes / 100) * IF(prestamo.tipo_pago_id = 2, 0.5, 1)), 2) AS cuota')
-                )
-                ->leftJoin('persona', 'prestamo.persona_id', '=', 'persona.id')
-                ->leftJoin('tipo_pago', 'prestamo.tipo_pago_id', '=', 'tipo_pago.id')
-                ->when($rol > 1, function ($query) use ($id_usuario) {
-                    $query->where('administrador', $id_usuario);
-                })
-                ->when($search, function ($query, $search) {
-                    $query->where('persona.nombre', 'like', '%' . $search . '%');
-                })
-                ->orderBy('prestamo.estado')
-                ->orderBy('prestamo.fecha', 'desc')
-                ->get();
+            ->select(
+                'prestamo.id',
+                DB::raw('LPAD(prestamo.codigo, 3, "0") as codigo'),
+                DB::raw('FORMAT(prestamo.cantidad, 2) as cantidad'),
+                DB::raw('FORMAT(prestamo.interes, 2) as interes'),
+                'prestamo.estado',
+                'prestamo.amortizacion',
+                'prestamo.id as comprobante',
+                'prestamo.administrador',
+                'prestamo.pago_especifico as pagoEspecifico',
+                'persona.nombre as persona',
+                'tipo_pago.nombre as tipoPago',
+                'prestamo.tipo_pago_id',
+                'prestamo.numero_pagos',
+                'prestamo.pago_especifico',
+                DB::raw('IFNULL(prestamo.observacion, "") as observacion'),
+                DB::raw('DATE_FORMAT(prestamo.fecha, "%d/%m/%Y") as fecha'),
+                DB::raw('IFNULL((SELECT remanente FROM recibo WHERE recibo.prestamo_id = prestamo.id and recibo.estado = 2  ORDER BY recibo.id DESC LIMIT 1), prestamo.cantidad) AS deuda'),
+                DB::raw('ROUND((prestamo.cantidad / prestamo.numero_pagos) + (prestamo.cantidad * (prestamo.interes / 100) *
+                IF(prestamo.tipo_pago_id = 2, 0.5, IF(prestamo.tipo_pago_id = 5, 0.25, 1))), 2) AS cuota')
+            )
+            ->leftJoin('persona', 'prestamo.persona_id', '=', 'persona.id')
+            ->leftJoin('tipo_pago', 'prestamo.tipo_pago_id', '=', 'tipo_pago.id')
+            ->when($rol > 1, function ($query) use ($id_usuario) {
+                $query->where('administrador', $id_usuario);
+            })
+            ->when($search, function ($query, $search) {
+                $query->where('persona.nombre', 'like', '%' . $search . '%');
+            })
+            ->orderBy('prestamo.estado')
+            ->orderBy('prestamo.fecha', 'desc')
+            ->get();
+
 
 
             foreach ($prestamos as $prestamo) {
