2beb7b3e1c137793ee47218818c338ad78720079
hhuezo <hugo.alex.huezo@gmail.com>
2024-08-24 08:20:16 -0600

    cerrar sesion

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1142,7 +1142,9 @@
 
                                                         <span class="ms-2">Settings </span>
                                                     </a>
-                                                    <a href="{{ url('/login') }}" class="dropdown-item ai-icon">
+                                                    <a   href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();" class="dropdown-item ai-icon">
                                                         <svg class="logout-svg" xmlns="http://www.w3.org/2000/svg"
                                                             width="18" height="18" viewBox="0 0 24 24"
                                                             fill="none" stroke="currentColor" stroke-width="2"
@@ -1158,6 +1160,11 @@
                                             </div>
 
                                         </div>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            style="display: none;">
+                                            @csrf
+                                        </form>
                                     </div>
                                 </li>
                             </ul>
@@ -1212,7 +1219,7 @@
                     </li>
 
                     @can('administracion')
-                        <li><a class="has-arrow "  aria-expanded="false">
+                        <li><a class="has-arrow " aria-expanded="false">
                                 <div class="menu-icon">
                                     <span class="material-icons">
                                         settings
@@ -1229,63 +1236,78 @@
                     @endcan
 
 
-                        <li><a href="{{ url('prestamo_web') }}" class="" aria-expanded="false">
+                    <li><a href="{{ url('prestamo_web') }}" class="" aria-expanded="false">
                             <div class="menu-icon">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_115_172)">
-                                    <path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M12 4.25933C12.1489 4.25921 12.3 4.29247 12.4426 4.36281C12.6398 4.46014 12.7994 4.61977 12.8967 4.81698L14.9389 8.95491L19.5054 9.61846C20.0519 9.69788 20.4306 10.2053 20.3512 10.7519C20.3196 10.9695 20.2171 11.1706 20.0596 11.3242L16.7553 14.5451L17.5353 19.0931C17.6287 19.6374 17.2631 20.1544 16.7188 20.2478C16.502 20.2849 16.279 20.2496 16.0844 20.1473L12 18V4.25933Z" fill="#4E5566"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4.25933V18L7.91559 20.1473C7.42675 20.4043 6.82212 20.2163 6.56512 19.7275C6.46278 19.5328 6.42746 19.3099 6.46464 19.0931L7.24469 14.5451L3.94036 11.3242C3.54487 10.9387 3.53678 10.3055 3.92228 9.91006C4.07579 9.75258 4.27693 9.65009 4.49457 9.61846L9.06104 8.95492L11.1032 4.81699C11.2773 4.46426 11.6316 4.25961 12 4.25933Z" fill="#90959F"/>
+                                        <path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M12 4.25933C12.1489 4.25921 12.3 4.29247 12.4426 4.36281C12.6398 4.46014 12.7994 4.61977 12.8967 4.81698L14.9389 8.95491L19.5054 9.61846C20.0519 9.69788 20.4306 10.2053 20.3512 10.7519C20.3196 10.9695 20.2171 11.1706 20.0596 11.3242L16.7553 14.5451L17.5353 19.0931C17.6287 19.6374 17.2631 20.1544 16.7188 20.2478C16.502 20.2849 16.279 20.2496 16.0844 20.1473L12 18V4.25933Z"
+                                            fill="#4E5566" />
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M12 4.25933V18L7.91559 20.1473C7.42675 20.4043 6.82212 20.2163 6.56512 19.7275C6.46278 19.5328 6.42746 19.3099 6.46464 19.0931L7.24469 14.5451L3.94036 11.3242C3.54487 10.9387 3.53678 10.3055 3.92228 9.91006C4.07579 9.75258 4.27693 9.65009 4.49457 9.61846L9.06104 8.95492L11.1032 4.81699C11.2773 4.46426 11.6316 4.25961 12 4.25933Z"
+                                            fill="#90959F" />
                                     </g>
                                     <defs>
-                                    <clipPath id="clip0_115_172">
-                                    <rect width="24" height="24" fill="white"/>
-                                    </clipPath>
+                                        <clipPath id="clip0_115_172">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
                                     </defs>
                                 </svg>
                             </div>
-                                <span class="nav-text">Prestamos</span>
-                            </a>
-                        </li>
+                            <span class="nav-text">Prestamos</span>
+                        </a>
+                    </li>
 
-                        <li><a href="{{ url('prestamo_fijo_web') }}" class="" aria-expanded="false">
+                    <li><a href="{{ url('prestamo_fijo_web') }}" class="" aria-expanded="false">
                             <div class="menu-icon">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_113_177)">
-                                    <path d="M17 4H6C4.79111 4 4 4.7 4 6V18C4 19.3 4.79111 20 6 20H18C19.2 20 20 19.3 20 18V7.20711C20 7.0745 19.9473 6.94732 19.8536 6.85355L17 4ZM17 11H7V4H17V11Z" fill="#90959F"/>
-                                    <path opacity="0.3" d="M14.5 4H12.5C12.2239 4 12 4.22386 12 4.5V8.5C12 8.77614 12.2239 9 12.5 9H14.5C14.7761 9 15 8.77614 15 8.5V4.5C15 4.22386 14.7761 4 14.5 4Z" fill="white"/>
+                                        <path
+                                            d="M17 4H6C4.79111 4 4 4.7 4 6V18C4 19.3 4.79111 20 6 20H18C19.2 20 20 19.3 20 18V7.20711C20 7.0745 19.9473 6.94732 19.8536 6.85355L17 4ZM17 11H7V4H17V11Z"
+                                            fill="#90959F" />
+                                        <path opacity="0.3"
+                                            d="M14.5 4H12.5C12.2239 4 12 4.22386 12 4.5V8.5C12 8.77614 12.2239 9 12.5 9H14.5C14.7761 9 15 8.77614 15 8.5V4.5C15 4.22386 14.7761 4 14.5 4Z"
+                                            fill="white" />
                                     </g>
                                     <defs>
-                                    <clipPath id="clip0_113_177">
-                                    <rect width="24" height="24" fill="white"/>
-                                    </clipPath>
+                                        <clipPath id="clip0_113_177">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
                                     </defs>
-                            </svg>
+                                </svg>
                             </div>
-                                <span class="nav-text">Prestamos fijos</span>
-                            </a>
+                            <span class="nav-text">Prestamos fijos</span>
+                        </a>
 
 
-                        </li>
+                    </li>
 
-                        <li><a href="{{ url('persona') }}" class="" aria-expanded="false">
+                    <li><a href="{{ url('persona') }}" class="" aria-expanded="false">
                             <div class="menu-icon">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_113_177)">
-                                    <path d="M17 4H6C4.79111 4 4 4.7 4 6V18C4 19.3 4.79111 20 6 20H18C19.2 20 20 19.3 20 18V7.20711C20 7.0745 19.9473 6.94732 19.8536 6.85355L17 4ZM17 11H7V4H17V11Z" fill="#90959F"/>
-                                    <path opacity="0.3" d="M14.5 4H12.5C12.2239 4 12 4.22386 12 4.5V8.5C12 8.77614 12.2239 9 12.5 9H14.5C14.7761 9 15 8.77614 15 8.5V4.5C15 4.22386 14.7761 4 14.5 4Z" fill="white"/>
+                                        <path
+                                            d="M17 4H6C4.79111 4 4 4.7 4 6V18C4 19.3 4.79111 20 6 20H18C19.2 20 20 19.3 20 18V7.20711C20 7.0745 19.9473 6.94732 19.8536 6.85355L17 4ZM17 11H7V4H17V11Z"
+                                            fill="#90959F" />
+                                        <path opacity="0.3"
+                                            d="M14.5 4H12.5C12.2239 4 12 4.22386 12 4.5V8.5C12 8.77614 12.2239 9 12.5 9H14.5C14.7761 9 15 8.77614 15 8.5V4.5C15 4.22386 14.7761 4 14.5 4Z"
+                                            fill="white" />
                                     </g>
                                     <defs>
-                                    <clipPath id="clip0_113_177">
-                                    <rect width="24" height="24" fill="white"/>
-                                    </clipPath>
+                                        <clipPath id="clip0_113_177">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
                                     </defs>
-                            </svg>
+                                </svg>
                             </div>
-                                <span class="nav-text">Personas </span>
-                            </a>
+                            <span class="nav-text">Personas </span>
+                        </a>
 
 
-                        </li>
+                    </li>
 
 
 
